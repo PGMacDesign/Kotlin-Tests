14aaf4ceafdcdb6778949742c014796fa0b2bd72
PGMacDesign <PGMacDesign@gmail.com>
2020-01-09 16:07:38 -0800

    Working through tutorials, currently broken due to reworking UI elements

---
+++ app/src/main/java/pgmacdesign/kotlintests/activities/NavigationCoreActivity.kt
@@ -0,0 +1,51 @@
+package pgmacdesign.kotlintests.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NavigationCoreActivity : AppCompatActivity() {
+
+    //region Vars
+
+    //endregion
+
+    //region UI
+
+    //endregion
+
+    //region On Create and Init
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    fun initVariables(){
+
+    }
+
+    fun initUI(){
+
+    }
+
+    fun initDefaults(){
+
+    }
+
+    //endregion
+
+    //region Override Methods
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    //endregion
+}
---
+++ app/src/main/java/pgmacdesign/kotlintests/misc/MyApplication.kt
@@ -0,0 +1,26 @@
+package pgmacdesign.kotlintests.misc
+
+import android.content.Context
+import androidx.multidex.MultiDexApplication
+
+class MyApplication : MultiDexApplication() {
+
+//    var instance: MyApplication? = null
+//    object context: Context;
+//
+//    init {
+//        context = MyApplication().applicationContext
+//    }
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//
+//    }
+//
+//    fun getContext() : Context {
+//        if(this.context == null){
+//            this.context = this.applicationContext
+//        }
+//        return this.context
+//    }
+}
